576efc485e562aaf085b0e2a1c55c391bb21a6a9
Andy Reyes <andy@andyjreyes.com>
2015-05-06 23:22:17 -0700

    Added the APLEarthquakeTableViewCell.swift file and class Added the Bridging Header for the project

---
+++ SeismicXML/APLEarthquakeTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  APLEarthquakeTableViewCell.swift
+//  SeismicXML
+//
+//  Created by Andy Reyes on 5/6/15.
+//  Copyright (c) 2015 Apple Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class APLEarthquakeTableViewCell : UITableViewCell
+{
+    
+}
