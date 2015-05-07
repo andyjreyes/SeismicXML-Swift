f1687a95bb6122f07156067f08015d15702e3dc3
Andy Reyes <andy@andyjreyes.com>
2015-05-07 05:41:42 -0700

    Added functions and dateFormatter variables. Ensured exact behavior between the Objective-C and Swift versions of imageForMagnitude by having it return a UIImage? optional. Lazy instantiated dateFormatter just like the Objective-C code. There is some leftover debug code for when I test the lazy instantiation. This will be removed in a future commit.

--- SeismicXML/APLEarthquakeTableViewCell.swift
+++ SeismicXML/APLEarthquakeTableViewCell.swift
@@ -11,11 +11,62 @@ import UIKit
 
 class APLEarthquakeTableViewSwiftCell: UITableViewCell
 {
+    // References to the subviews which display the earthquake data.
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var magnitudeLabel: UILabel!
     @IBOutlet weak var magnitudeImage: UIImageView!
     
+    // Lazily instantiated NSDateFormatter
+    lazy private var dateFormatter: NSDateFormatter =
+    {
+        var tempFormatter: NSDateFormatter = NSDateFormatter()
+        
+        tempFormatter.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+        tempFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        tempFormatter.timeStyle = NSDateFormatterStyle.MediumStyle
+        
+        println("We are running dateFormatter's initialization block")
+        
+        return tempFormatter;
+        }()
     
     
+    internal func configureWithEarthquake(earthquake: APLEarthquake)
+    {
+        locationLabel.text = earthquake.location
+        dateLabel.text = dateFormatter.stringFromDate(earthquake.date)
+        magnitudeLabel.text = String(format: "%.1f", Double(earthquake.magnitude))
+        
+        if let image = imageForMagnitude(earthquake.magnitude)
+        {
+            magnitudeImage.image = image
+        }
+        
+        // See if the statements below run the dateFormatter: block multiple times
+//        dateFormatter.timeStyle = NSDateFormatterStyle.LongStyle
+//        dateFormatter.timeStyle = NSDateFormatterStyle.MediumStyle
+    }
+    
+    // Based on the magnitude of the earthquake, return an image indicating its seismic strength.
+    private func imageForMagnitude(magnitude: CGFloat) -> UIImage?
+    {
+        switch magnitude
+        {
+        case 5...CGFloat.max:
+            return UIImage(named: "5.0.png")
+            
+        case 4..<5:
+            return UIImage(named: "4.0.png")
+            
+        case 3..<4:
+            return UIImage(named: "3.0.png")
+            
+        case 2..<3:
+            return UIImage(named: "2.0.png")
+            
+        default:
+            return nil
+        }
+    }
 }
